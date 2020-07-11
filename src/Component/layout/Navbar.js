f9f1430b6d071a8ad9da008cc61d8d629e4ee14f
rajnish8869 <35104535+rajnish8869@users.noreply.github.com>
2020-07-11 18:29:23 +0530

    Update Navbar.js

--- src/Component/layout/Navbar.js
+++ src/Component/layout/Navbar.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({ icon, title }) => {
         return (
-        <nav className="navbar" style="background: #000;">
+        <nav className="navbar bg-primary">
             <h1><i className={icon}/> {title}</h1>
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
             </ul>
-            
+
         </nav>
         )
     }
@@ -20,10 +20,11 @@ const Navbar = ({ icon, title }) => {
 Navbar.defaultProps = {
     title : "Github finder",
     icon : 'fab fa-github'
-    
+
 }
 Navbar.propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired
 }
+
 export default Navbar
