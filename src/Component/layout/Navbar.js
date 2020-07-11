839a91ed961990e46105ce0e01daae9c886df78d
rajnish8869 <35104535+rajnish8869@users.noreply.github.com>
2020-07-11 18:32:55 +0530

    Update Navbar.js

--- src/Component/layout/Navbar.js
+++ src/Component/layout/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({ icon, title }) => {
         return (
-        <nav className="navbar bg-primary">
+        <nav className="navbar bg-dark">
             <h1><i className={icon}/> {title}</h1>
             <ul>
                 <li><Link to='/'>Home</Link></li>
