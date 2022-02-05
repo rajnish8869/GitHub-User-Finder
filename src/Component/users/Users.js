d03262ee986bd93268ad700959dcd1eb3c07ed8e
rajnish8869 <35104535+rajnish8869@users.noreply.github.com>
2022-02-05 16:42:19 +0530

    final change

--- src/Component/users/Users.js
+++ src/Component/users/Users.js
@@ -1,32 +1,41 @@
-import React from 'react'
-import UserItem from './UserItem'
-import Spinner from '../layout/Spinner'
-import PropTypes from 'prop-types'
+import React from "react";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-const Users = ({ loading , users}) => {
+const Users = ({ loading, users, random }) => {
+  console.log("users", users);
 
-            if (loading){
-            return <Spinner />
-            }else{
-                return (
-                <div style={userStyle}>
-                {users.map(user => (
-                <UserItem key={user.id} user={user} />
-                ))}
-                </div>
-                )
-            }
-    };
+  if (users.length === 0) {
+    return (
+      <div style={userStyle}>
+        {random.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
 
 Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
-}
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit,minmax(18rem, 1fr) )',
-    gridGap: '1rem',
-}
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit,minmax(18rem, 1fr) )",
+  gridGap: "1rem",
+};
 
-export default Users
+export default Users;
